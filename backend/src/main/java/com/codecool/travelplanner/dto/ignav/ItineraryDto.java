07a236faffa0729dc5828a8fe9bd8b47560340d2
marton-kovacs-0 <98marci@gmail.com>
2026-07-29 11:39:00 +0200

    Implemented ItineraryDto record

--- backend/src/main/java/com/codecool/travelplanner/dto/ignav/ItineraryDto.java
+++ backend/src/main/java/com/codecool/travelplanner/dto/ignav/ItineraryDto.java
@@ -1,4 +1,11 @@
 package com.codecool.travelplanner.dto.ignav;
 
-public record ItineraryDto() {
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record ItineraryDto(
+        PriceDto price,
+        OutboundDto outbound,
+        @JsonProperty("cabin_class") String cabinClass,
+        @JsonProperty("requires_self_transfer") boolean requiresSelfTransfer,
+        @JsonProperty("ignav_id") String ignavId) {
 }
---
+++ backend/src/main/java/com/codecool/travelplanner/dto/ignav/PriceDto.java
@@ -0,0 +1,4 @@
+package com.codecool.travelplanner.dto.ignav;
+
+public record PriceDto() {
+}
