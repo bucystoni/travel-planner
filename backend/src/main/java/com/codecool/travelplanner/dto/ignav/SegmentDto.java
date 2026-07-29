f5d03232ad509d89d784f860112868bab8a80262
marton-kovacs-0 <98marci@gmail.com>
2026-07-29 11:59:31 +0200

    Implemented SegmentsDto record

--- backend/src/main/java/com/codecool/travelplanner/dto/ignav/SegmentDto.java
+++ backend/src/main/java/com/codecool/travelplanner/dto/ignav/SegmentDto.java
@@ -1,4 +1,46 @@
 package com.codecool.travelplanner.dto.ignav;
 
-public record SegmentDto() {
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record SegmentDto(
+
+        @JsonProperty("marketing_carrier_code")
+        String marketingCarrierCode,
+
+        @JsonProperty("flight_number")
+        String flightNumber,
+
+        @JsonProperty("operating_carrier_name")
+        String operatingCarrierName,
+
+        @JsonProperty("departure_airport")
+        String departureAirport,
+
+        @JsonProperty("departure_time_local")
+        String departureTimeLocal,
+
+        @JsonProperty("departure_timezone")
+        String departureTimezone,
+
+        @JsonProperty("departure_time_utc")
+        String departureTimeUtc,
+
+        @JsonProperty("arrival_airport")
+        String arrivalAirport,
+
+        @JsonProperty("arrival_time_local")
+        String arrivalTimeLocal,
+
+        @JsonProperty("arrival_timezone")
+        String arrivalTimezone,
+
+        @JsonProperty("arrival_time_utc")
+        String arrivalTimeUtc,
+
+        @JsonProperty("duration_minutes")
+        int durationMinutes,
+
+        String aircraft
+
+) {
 }
