9e12c96081c2748cfab37a7e1d217a2823cffc32
kira-khern <kkhern32@gmail.com>
2026-08-11 07:06:22 +0200

    Rename WeatherApiResponse.list to forecasts and use getFirst()

--- backend/src/main/java/com/codecool/travelplanner/dto/weather/WeatherApiResponse.java
+++ backend/src/main/java/com/codecool/travelplanner/dto/weather/WeatherApiResponse.java
@@ -1,9 +1,10 @@
 package com.codecool.travelplanner.dto.weather;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record WeatherApiResponse(List<ForecastEntry> list) {
+public record WeatherApiResponse(@JsonProperty("list") List<ForecastEntry> forecasts) {
 }
