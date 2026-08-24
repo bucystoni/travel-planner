958468467ebb6543405a99fbb69c0b40a75cf8a2
Telegdy Luca <telegdyluca@gmail.com>
2026-08-24 12:02:19 +0200

    Implement UserRepository

---
+++ backend/src/main/java/com/codecool/travelplanner/repository/user/UserRepository.java
@@ -0,0 +1,15 @@
+package com.codecool.travelplanner.repository.user;
+
+import com.codecool.travelplanner.model.entity.user.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findUserByUsername(String username);
+
+    Optional<UserEntity> findUserByEmail(String email);
+}
