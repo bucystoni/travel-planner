d26b90e7b47fe00bc73664c82dd4a8a03cc60f80
bucystoni <bucys.tony@gmail.com>
2026-08-25 13:35:46 +0200

    Added /destinations endpoint for city search

--- backend/src/main/java/com/codecool/travelplanner/controller/DestinationsController.java
+++ backend/src/main/java/com/codecool/travelplanner/controller/DestinationsController.java
@@ -1,9 +1,12 @@
 package com.codecool.travelplanner.controller;
 
 import com.codecool.travelplanner.api.DestinationsApi;
+import com.codecool.travelplanner.mapper.places.PlacesMapper;
+import com.codecool.travelplanner.model.City;
 import com.codecool.travelplanner.model.FlightOfferDto;
 
 import com.codecool.travelplanner.service.flight.FlightService;
+import com.codecool.travelplanner.service.places.PlacesService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,19 +16,16 @@ import java.util.List;
 @RestController
 public class DestinationsController implements DestinationsApi {
 
-    private final FlightService flightService;
+    private final PlacesService placesService;
 
-    public DestinationsController(FlightService flightService) {
-        this.flightService = flightService;
+    public DestinationsController(PlacesService placesService) {
+        this.placesService = placesService;
     }
 
     @Override
-    public ResponseEntity<List<FlightOfferDto>> destinationsGet(String destinationIataCode, String departureIataCode, LocalDate date) {
-        List<FlightOfferDto> tickets = flightService.getFlightOffers(
-                departureIataCode,
-                destinationIataCode,
-                date);
-        return ResponseEntity.ok(tickets);
+    public ResponseEntity<List<City>> destinationsGet(String cityName) {
+        City city = PlacesMapper.convertResponseToCity(placesService.searchCity(cityName));
+        return ResponseEntity.ok(List.of(city));
     }
 
 
