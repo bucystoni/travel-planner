0f915c2b3b27e0b79b07f733f9e475cf9ad99599
kira-khern <kkhern32@gmail.com>
2026-08-26 13:43:55 +0200

    Add custom exception handling for registration and login

--- backend/src/main/java/com/codecool/travelplanner/controller/advice/AuthControllerAdvice.java
+++ backend/src/main/java/com/codecool/travelplanner/controller/advice/AuthControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.codecool.travelplanner.controller.advice;
 
+import com.codecool.travelplanner.exception.InvalidCredentialsException;
 import com.codecool.travelplanner.exception.UserAlreadyExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +13,13 @@ public class AuthControllerAdvice {
     @ExceptionHandler(UserAlreadyExistsException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     public String userAlreadyExistsExceptionHandler(UserAlreadyExistsException ex) {
+        return ex.getMessage();
+    }
+
+
+    @ExceptionHandler(InvalidCredentialsException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public String invalidCredentialsExceptionHandler(InvalidCredentialsException ex) {
         return ex.getMessage();
     }
 }
