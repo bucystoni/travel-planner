6954c0af7cfa4a05e29f3a9e24bbc3c092f0916a
Telegdy Luca <telegdyluca@gmail.com>
2026-08-26 12:00:02 +0200

    Fix frontend authentication setup

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -7,7 +7,7 @@ export default function NavBar() {
             <nav>
                 <Link to={"/registration"}>Register</Link>
                 <Link to={"/login"}>Login</Link>
-                <Link to={"/destinations"}>Flights</Link>
+                <Link to={"/flights"}>Flights</Link>
                 <Link to={"/accommodations"}>Accommodations</Link>
                 <Link to={"/restaurants"}>Restaurants</Link>
                 <Link to={"/sights"}>Sights</Link>
