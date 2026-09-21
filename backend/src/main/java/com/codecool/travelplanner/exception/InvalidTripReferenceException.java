171aef5c9f6700446c9d06100e9880d3ece9f104
kira-khern <kkhern32@gmail.com>
2026-09-21 13:48:32 +0200

    Return 400 instead of 404 when trip references a non-existent place

---
+++ backend/src/main/java/com/codecool/travelplanner/exception/InvalidTripReferenceException.java
@@ -0,0 +1,7 @@
+package com.codecool.travelplanner.exception;
+
+public class InvalidTripReferenceException extends RuntimeException {
+    public InvalidTripReferenceException(String message) {
+        super(message);
+    }
+}
